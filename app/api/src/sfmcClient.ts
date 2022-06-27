d08e0e111859e3eac86e31b402bce6641489159c
Hari <harraj4568@gmail.com>
2022-06-27 17:51:28 +0530

    node modules

--- app/api/src/sfmcClient.ts
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-export const sfmcClient = axios.create({});
--- app/ui/src/routes/settings/EmbedForWeb.tsx
+++
@@ -1,3 +0,0 @@
-export function EmbedForWeb() {
-    return <div className="slds-text-heading_medium">Embed for web</div>;
-}
